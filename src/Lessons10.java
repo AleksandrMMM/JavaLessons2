b499f09a1934a6699d388c4b9bebaacaeb7638dc
aleks <girbov@gmail.com>
2020-11-15 19:55:53 +0300

    test

--- src/Lessons10.java
+++ src/Lessons10.java
@@ -1,7 +1,8 @@
 public class Lessons10 {
     public static void main(String[] args) {
         Book book1 = new Book(Types.ROMAN,"War and Piace", "Lev Tolstoy");
-        System.out.println(book1.getBookTypes() + " " + book1.getName());
+        System.out.println(book1.getBookTypes() + " " + book1.getName() + " " + book1.getAuthor());
 
     }
 }
+
